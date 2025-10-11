da8430798fd979ed9a3137c9ddcc59dc812b121b
shazmuju <147649533+shazmuju@users.noreply.github.com>
2025-10-11 12:46:29 +0530

    Time: 15 ms (11.52%) | Memory: 39.5 MB (12.73%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,13 +1,16 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        if(x<0) return false;
-        int revnum=0;
-        int num=x;
-        while(x>0){
-            revnum=revnum*10+x%10;
-            x=x/10;
-        }
-      return revnum==num;
+        // if(x<0) return false;
+        // int revnum=0;
+        // int num=x;
+        // while(x>0){
+        //     revnum=revnum*10+x%10;
+        //     x=x/10;
+        // }
+
+      string s = x.ToString();
+      string reversed = new string(s.Reverse().ToArray());
+      return s == reversed;
     }
      
 }
