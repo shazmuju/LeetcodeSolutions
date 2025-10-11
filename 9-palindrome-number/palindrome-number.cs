dc0a8a7ff5e5d9007cb871f0b2d7371fb3b10e3f
shazmuju <147649533+shazmuju@users.noreply.github.com>
2025-10-11 12:06:41 +0530

    Time: 1 ms (99.95%) | Memory: 33.9 MB (99.79%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x<0) return false;
+        int revnum=0;
+        int num=x;
+        while(x>0){
+            revnum=revnum*10+x%10;
+            x=x/10;
+        }
+      return revnum==num;
+    }
+     
+}
