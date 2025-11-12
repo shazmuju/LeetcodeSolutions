8d9a28f42ce36452d788c45f9e7fb71c35a8ff6c
shazmuju <147649533+shazmuju@users.noreply.github.com>
2025-11-12 12:46:06 +0530

    Time: 4 ms (47.88%) | Memory: 49.3 MB (62.21%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+     IList<bool> Candylist=new List<bool>();
+     int Maxcandies=candies.Max();
+    for(int i=0;i<candies.Length;i++){
+        if(candies[i]+extraCandies>=Maxcandies){
+            Candylist.Add(true);
+        }
+      else{
+           Candylist.Add(false);
+      }
+    }
+    return Candylist;
+    }
+}
