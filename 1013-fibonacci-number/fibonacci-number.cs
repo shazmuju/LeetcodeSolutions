8e4afaa0fd40ac7480f70cd07951785b639401cd
shazmuju <147649533+shazmuju@users.noreply.github.com>
2025-11-27 13:31:14 +0530

    Time: 20 ms (91.66%) | Memory: 29 MB (44.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int Fib(int n) {
-     if(n<=1){
+    if(n<=1){
         return n;
-     } 
+    }
 
-     int [] fibn =new int[n+1];
-     fibn[0]=0;
-     fibn[1]=1;
+    int a=0;
+    int b=1;
+    
 
-     for(int i=2;i<=n;i++){
-        fibn[i]=fibn[i-1]+fibn[i-2];
-     }
-
-     return fibn[n];
+    for(int i=2;i<=n;i++){
+        int c=a+b;
+        a=b;
+        b=c;
+    }
+    
+    return b;
     }
 }
