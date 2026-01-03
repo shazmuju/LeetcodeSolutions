1b1cceb3ffe4d4569d576da9125fea1ee511c57d
shazmuju <147649533+shazmuju@users.noreply.github.com>
2026-01-03 14:58:43 +0530

    Time: 1 ms (100.00%) | Memory: 49.6 MB (53.54%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+       if(n==0){
+        return true;
+       }
+       for(int i=0;i<flowerbed.Length;i++){
+            if(flowerbed[i]==0){
+                if((i==0 || flowerbed[i-1]==0) && (i==flowerbed.Length-1 || flowerbed[i+1]==0)){
+                    flowerbed[i]=1;
+                    n--;
+                }
+                if(n==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+}
