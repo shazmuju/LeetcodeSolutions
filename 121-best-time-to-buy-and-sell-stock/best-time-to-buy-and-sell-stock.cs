83003a253e86239fa544bbd97e8c93e262998dd2
shazmuju <147649533+shazmuju@users.noreply.github.com>
2026-02-10 21:02:45 +0530

    Time: 1 ms (99.97%) | Memory: 59.7 MB (27.99%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+       int buy_price=prices[0];
+
+       int profit=0;
+
+       for(int i=1;i<prices.Length;i++){
+        if(prices[i]<buy_price){
+            buy_price=prices[i];
+        }
+        else{
+            int current_profit=prices[i]-buy_price;
+            profit=Math.Max(current_profit,profit);
+        }
+        
+       } 
+       return profit;
+    }
+}
