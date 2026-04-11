0b92bccdb071864c7334176995721e2a5e6287f7
shazmuju <147649533+shazmuju@users.noreply.github.com>
2026-04-11 08:27:37 +0530

    Time: 15 ms (92.65%) | Memory: 28.7 MB (79.41%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
@@ -1,17 +1,7 @@
 public class Solution {
     public int NumberOfMatches(int n) {
-        int matches=0;
-        while(n>1){
-            if(n%2==0){
-                matches+=n/2;
-                n/=2;
-            }
-            else{
-                matches+=(n-1)/2;
-                n=(n - 1) / 2 + 1;
-            }
-        }
-        return matches;
+       return n-1;
+       
 
     }
 }
