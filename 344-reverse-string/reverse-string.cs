85017fdc1d723e02e9ce4c4df0aecc6684734817
shazmuju <147649533+shazmuju@users.noreply.github.com>
2026-05-08 07:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 64.5 MB (91.72%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+     int left=0;
+     int right=s.Length-1;
+
+    while(left<right)
+    {
+       char temp=s[left];
+       s[left]=s[right];
+       s[right]=temp;
+
+       left++;
+       right--;
+     }
+      
+        //Array.Reverse(s);
+      
+    }
+}
