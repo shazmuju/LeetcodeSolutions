34849a15aff68233e6170096984c96f9801cca16
shazmuju <147649533+shazmuju@users.noreply.github.com>
2026-05-09 07:46:50 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (80.00%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] GetNoZeroIntegers(int n) {
+        for(int i=1;i<n;i++){
+            int a=i;
+            int b=n-i;
+
+            if(!Containzero(a)&&!Containzero(b)){
+                return new int [] {a,b};
+            }
+
+        }
+        return new int[] {};
+
+    }
+
+    private bool Containzero(int num){
+       while(num>0){
+        if(num%10==0){
+            return true;
+        }
+        num/=10;
+       }
+       return false;
+    }
+}
